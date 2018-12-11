3aa7b08107e3a2005dc613f7a0cff831f4a1bfde
Eunchan Park <freetime4y@gmail.com>
2018-12-11 21:54:36 +0200

    framebuffer_width and height change

--- pisetup.sh
+++ pisetup.sh
@@ -4,14 +4,12 @@
 sed -i 's/#disable_overscan/disable_overscan/g' /boot/config.txt
 
 # I need to check whether it is right or not.
-#sed -i 's/framebuffer_width=/framebuffer_width=1280  # /g' /boot/config.txt
-#sed -i 's/framebuffer_height=/framebuffer_height=720  # /g' /boot/config.txt
-
+sed -i 's/#framebuffer_width=/framebuffer_width=1280  # /g' /boot/config.txt
+sed -i 's/#framebuffer_height=/framebuffer_height=720  # /g' /boot/config.txt
 
 
 wget https://www.realvnc.com/download/file/vnc.files/VNC-Server-6.3.2-Linux-ARM.deb && dpkg -i ./VNC-Server-6.3.2-Linux-ARM.deb && systemctl enable vncserver-x11-serviced.service #systemd 
 
 
 #wget https://raw.githubusercontent.com/ROBOTIS-GIT/robotis_tools/master/install_ros_kinetic_rp3.sh && chmod 755 ./install_ros_kinetic_rp3.sh && bash ./install_ros_kinetic_rp3.sh
-
 
