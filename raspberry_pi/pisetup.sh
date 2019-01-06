70a81a20dde3e82fcc14ce8b3876a16970c6ec7c
Eunchan Park <freetime4y@gmail.com>
2019-01-06 08:28:02 +0200

    enable R-PI SSH

--- raspberry_pi/pisetup.sh
+++ raspberry_pi/pisetup.sh
@@ -2,6 +2,12 @@
 
 #set -o errexit #need to test
 
+# 1. connect the same wifi
+
+# 2. ssh enable: this will be the first thing we need to go
+sudo systemctl enable ssh
+sudo service ssh restart
+
 #display to 1280 x 720
 #sed -i 's/#disable_overscan/disable_overscan/g' /boot/config.txt
 #sed -i 's/#framebuffer_width=/framebuffer_width=1280  # /g' /boot/config.txt
@@ -18,4 +24,5 @@
 #https://pimylifeup.com/raspberry-pi-visual-studio-code/
 wget https://packagecloud.io/headmelted/codebuilds/gpgkey -O - | sudo apt-key add -
 curl -L https://code.headmelted.com/installers/apt.sh | sudo bash
+
 
